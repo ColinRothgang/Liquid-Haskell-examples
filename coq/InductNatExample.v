85a9925006c7104d5d792b0d4a452cd529d155e9
Colin Rothgang <colin.rothgang@imdea.org>
2023-10-27 20:11:42 +0200

    Improved tactics and more theorems translated to Coq

--- coq/InductNatExample.v
+++ coq/InductNatExample.v
@@ -1,5 +1,4 @@
-Ltac smt_trivial := simpl; first [ assumption | intuition discriminate | easy].
-Ltac by_arg arg := simpl; rewrite arg; trivial.
+Load LHCoqTactics.
 
 Inductive N:Set := Z : N | Suc: N -> N.
 
@@ -28,7 +27,7 @@ Theorem add_zero_r (n: N): add n Z = n.
 Proof.
   induction n.
   - smt_trivial.
-  - by_arg IHn.
+  - smt_app IHn.
 Qed.
 
 (** Addition with right sucessor *)
@@ -36,5 +35,200 @@ Theorem add_suc_r (m: N) (n: N): Suc (add m n) = add m (Suc n).
 Proof.
   induction m as [|m IHm].
   - smt_trivial.
-  - by_arg IHm.
+  - smt_app IHm.
 Qed.
+
+(** Addition commutes *)
+Theorem add_comm (m:N) (n:N): add m n = add n m.
+Proof.
+  induction m as [|m IHm].
+  - smt_app add_zero_r.
+  - smt_app IHm. smt_app add_suc_r.
+Qed.
+
+(* Addition  is associative *)
+Theorem add_assoc (m:N) (n:N) (o:N): add m (add n o) = add (add m n) o.
+Proof.
+  induction m as [|m IHm].
+  - smt_trivial.
+  - smt_app IHm.
+Qed.
+
+
+(** Multiplication of natural numbers *)
+Fixpoint mult m n :=
+  match m with
+  | Z => Z
+  | Suc m => add n (mult m n)
+end.
+
+(** Multiplication definition lemmas: *)
+Theorem mult_zero_l (n:N): mult Z n = Z.
+Proof.
+  induction n as [|n IHn].
+  - smt_trivial.
+  - smt_app IHn.
+Qed.
+
+Theorem mult_suc_l (m:N) (n:N): mult (Suc m) n = add n (mult m n).
+Proof.
+  induction m as [|m IHm].
+  - smt_trivial.
+  - smt_trivial.
+Qed.
+
+(** Multiplication with right zero *)
+Theorem mult_zero_r (n:N): mult n Z = Z.
+Proof.
+  induction n as [|n IHn].
+  - smt_trivial.
+  - smt_app IHn.
+Qed.
+
+(** Multiplication with right sucessor *)
+Theorem mult_suc_r (n:N) (m:N): mult n (Suc m) = add n (mult n m).
+Proof.
+  induction n as [|n IHn].
+  - smt_trivial.
+  - smt_app IHn. 
+    
+    (** Obvious translation, nor working:
+    smt_app add_assoc.
+    smt_app add_comm. 
+    smt_app add_assoc.
+    *)
+    smt_app_with3 add_assoc (m) (n) (mult n m).
+    smt_app_with3 add_assoc n m (mult n m).
+    smt_app_with2 add_comm m n. 
+Qed.
+
+Definition one := Suc Z.
+Definition two := Suc one.
+
+(** Multiplication with right 1 *)
+Theorem mult_one_r (n:N): mult n one = n.
+Proof.
+  induction n as [|n IHn].
+  - smt_trivial.
+  - smt_app IHn. 
+Qed.
+
+(** Multiplication with left 1 *)
+Theorem mult_one_l (n:N): mult one n = n.
+Proof.
+  induction n as [|n IHn].
+  - smt_trivial.
+  - smt_app add_zero_r.
+Qed.
+
+(** Addition distributes over right multiplication *)
+Theorem add_dist_rmult (m:N) (n:N) (o:N): mult (add m n) o = add (mult m o) (mult n o).
+Proof.
+  induction m as [|m IHm].
+  - smt_trivial.
+  - smt_app IHm. 
+    (** rewrite <- add_assoc with o (mult m o) (mult n o).*)
+    smt_app_with3 add_assoc o (mult m o) (mult n o).
+Qed.
+
+(** Addition distributes over left multiplication *)
+Theorem add_dist_lmult (m:N) (n:N) (o:N): mult m (add n o) = add (mult m n) (mult m o).
+Proof.
+  induction n as [|n IHn].
+  - smt_app_with mult_zero_r m.
+  - smt_app_with2 mult_suc_r m (add n o).
+    smt_app IHn.
+    smt_app_with3 add_assoc m (mult m n) (mult m o).
+    smt_app_with2 mult_suc_r m n.
+Qed.
+
+Theorem mult_assoc (m:N) (n:N) (o:N): mult (mult m n) o = mult m (mult n o).
+Proof.
+  induction m as [|m IHm].
+  - smt_trivial.
+  - smt_app_with3 add_dist_rmult n (mult m n) o. 
+    smt_app IHm.
+Qed.
+
+Fixpoint eqN m n :=
+  match (m, n) with
+  | (Z, Z) => True
+  | (Suc m, Suc n) => eqN m n
+  | _ => False
+end.
+
+Fixpoint geqN m n :=
+  match (m, n) with
+  | (_, Z) => True
+  | (Z, Suc n) => False
+  | (Suc m, Suc n) => geqN m n
+end.
+
+Fixpoint geN m n :=
+  match (m, n) with
+  | (Z, _) => False
+  | (Suc _, Z) => True
+  | (Suc m, Suc n) => geN m n
+end.
+
+
+(** Two issues are demonstrated in the subsequent lemma:
+In the first inductive case (base case) we need to destruct n, for the proof to succeed
+In the second case, the (necessary) simplification following first tactic already
+solves the proof goal, so the second argument must *not* be translated from LH to Coq
+*)
+
+Theorem add_mono_r (m:N) (n:N): ((geqN (add m n) m) /\ (geN n Z -> geN (add m n) m)).
+Proof.
+  induction m as [|m IHm].
+  - destruct n; ple.
+  - smt_app_with2 add_suc_r m n. 
+Qed.
+
+Theorem add_mono_l (m:N) (n:N): geqN (add m n) n /\ (geN m Z -> geN (add m n) n).
+Proof.
+  smt_app_with2 add_comm m n.
+  smt_app add_mono_r.
+(**
+  For reasons I don't understand, the below tactic doesn't succeed (using apply with directly gives error:
+  not the right number of missing arguments (expected 0),
+  so it looks as though apply doesn't accept argument for the theorem that does take arguments):
+  smt_app_with2 add_mono_r (m:=n) (n:=m).*)
+Qed.
+
+(** The following theorem is rather hard to translate:
+   For start we need to do induction on both m and n,
+   so we reintroduce them as universally quantified variables with generalize dependent (in order opposite to the order in which we do induction),
+   then we do induction on first m then n, immediately finishing the trivial cases (they are all trivial in LH, so it is clear in translation which cases still need to be treated)
+   Then, we need to split the goal (done by split_ple the firststep in smt_app_ih).
+   Finally we move the antecedent into the context as a hypothesis, simplify it (using injection_in)
+   and finish the proof with an invocation of smt_app IHm, the first and (due to the generalize dependents) stronger induction principle
+*)
+Theorem eq_equal (m:N) (n:N): eqN m n <-> m = n.
+Proof.
+  generalize dependent n. generalize dependent m.
+  induction m as [|m IHm]; induction n as [|n IHn]; try smt_trivial.
+  smt_app_ih IHm.
+Qed.
+
+(** Here for some reason the smt_app_with2 tactics don't work, maybe because the rewrite default to a setoid_rewrite and those don't support the withs *)
+Theorem eq_sym (m:N) (n:N): eqN m n <-> eqN n m.
+Proof.
+  split.
+  - smt_app eq_equal. smt_app eq_equal.
+  - smt_app eq_equal. smt_app eq_equal.
+Qed.  
+
+Theorem mult_zero (m:N) (n:N): m <> Z /\ n <> Z -> geqN (mult m n) n.
+Proof.
+  induction m as [|m IHm].
+  - smt_trivial.
+  - smt_app_with2 mult_suc_l m n.
+    smt_app_with2 add_comm n (mult m n).
+    smt_app_with ge_zero n.
+    smt_app_with2 add_mono_l (mult m n) n.
+    smt_app_with2 ge_geq (add (mult m n) n) n.
+Qed.
+
+
+
---
+++ coq/LHCoqTactics.v
@@ -0,0 +1,130 @@
+Require Import Notations.
+Require Import Logic.
+Require Import Specif.
+
+(** to enable rewriting using other relations not just = *)
+Require Import Setoid.
+(** to rewrite using <-> *)
+Require Import Classes.Morphisms_Prop.
+
+Require Export Init.Tactics.
+
+
+(** The first two tactics are taken from https://gitlab.mpi-sws.org/iris/stdpp/-/blob/df33944852793fd7a93368b6b0251e9f29a3c4dd/stdpp/tactics.v#L45-78 (they are BSD licensed).*)
+
+Ltac fast_done :=
+  solve
+    [ eassumption
+    | symmetry; eassumption
+    | reflexivity ].
+
+Tactic Notation "fast_by" tactic(tac) := tac; fast_done.
+(** mimicks Haskell's $ operator in Coq *)
+Notation "f $ x" := (f x) (at level 60, right associativity, only parsing).
+
+Ltac f_equal_ind :=
+  match goal with
+  | [ |- ?G ] =>
+    tryif
+      (tryif assert (~ G); [ injection |]
+       then fail else idtac)
+    then
+      fail "Not an inductive constructor"
+    else
+      f_equal
+  end.
+
+Tactic Notation "if_not_done" tactic(tac) := tryif simpl then tac else idtac.
+
+Ltac simpl_loop :=
+  repeat first
+    [ fast_done
+    | solve [trivial]
+    | f_equal_ind
+    | progress intros
+    | solve [symmetry; trivial]
+    | discriminate
+    | contradiction
+    | intuition discriminate
+    | congruence
+    | exfalso
+    | intros; exfalso
+    | split].
+
+(*Require Import SMTCoq.SMTCoq.
+Require Import Bool.
+
+Require Import ZArith.
+
+Import BVList.BITVECTOR_LIST.
+From Sniper Require Import Sniper. *)
+
+Ltac injectivity_in H := injection H; clear H; intros H.
+  
+Ltac ple := simpl; first [ intuition discriminate | try congruence | simpl_loop | eauto]; try f_equal_ind.
+Local Ltac split_ple := ple; split; ple.
+Local Ltac intros_ple :=
+  let H' := fresh "H" in
+  split_ple; intros H'; try injectivity_in H'; simpl in H'.
+
+Ltac smt_trivial := simpl; first [ assumption | intuition discriminate | easy ].
+
+Tactic Notation "smt_ple_tac" tactic(tac) :=
+  first [ tac | ple; tac | split_ple; tac].
+Local Ltac smt_ap th := smt_ple_tac apply th.
+Local Ltac smt_ap_with th arg := smt_ple_tac apply th with arg.
+Local Ltac smt_ap_with2 th arg arg2 := smt_ple_tac apply th with arg arg2.
+Local Ltac smt_ap_with3 th arg arg2 arg3 := smt_ple_tac apply th with arg arg2 arg3.
+
+Local Ltac smt_rw th := smt_ple_tac rewrite th.
+Local Ltac smt_rw_with th arg := smt_ple_tac rewrite th with arg.
+Local Ltac smt_rw_with2 th arg arg2 := smt_ple_tac rewrite th with arg arg2.
+Local Ltac smt_rw_with3 th arg arg2 arg3 := smt_ple_tac rewrite th with arg arg2 arg3.
+
+Local Ltac smt_rwr th := smt_ple_tac rewrite -> th.
+Local Ltac smt_rwr_with th arg := smt_ple_tac rewrite <- th with arg.
+Local Ltac smt_rwr_with2 th arg arg2 := smt_ple_tac rewrite <- th with arg arg2.
+Local Ltac smt_rwr_with3 th arg arg2 arg3 := smt_ple_tac rewrite <- th with arg arg2 arg3.
+
+Tactic Notation "smt_use_rw_rwr_ap" tactic(appl_tac) tactic(rw_tac) tactic(rwr_tac) :=
+  if_not_done (first [progress rw_tac | progress rwr_tac | appl_tac]).
+
+Ltac smt_use th := smt_use_rw_rwr_ap (smt_ap th) (smt_rw th) (smt_rwr th).
+Ltac smt_use_with th arg := smt_use_rw_rwr_ap (smt_ap_with th arg) (smt_rw_with th arg) (smt_rwr_with th arg).
+Ltac smt_use_with2 th arg arg2 := smt_use_rw_rwr_ap (smt_ap_with2 th arg arg2) (smt_rw_with2 th arg arg2) (smt_rwr_with2 th arg arg2).
+Ltac smt_use_with3 th arg arg2 arg3:= smt_use_rw_rwr_ap (smt_ap_with3 th arg arg2 arg3) (smt_rw_with3 th arg arg2 arg3) (smt_rwr_with3 th arg arg2 arg3).
+
+Ltac smt_app th :=
+  (** first [ rewrite th | ple; rewrite th | split_ple; rewrite th | rewrite <- th | ple; rewrite <- th | split_ple; rewrite <- th| apply th | ple; apply th | split_ple; apply th]; try smt_trivial.*)
+  smt_use th; if_not_done (try smt_trivial).
+Tactic Notation "smt_app_with" constr(th) constr(arg) := (**first [ rewrite th with arg | ple; rewrite th with arg | split_ple; rewrite th with arg | rewrite <- th with arg | ple; rewrite <- th with arg | split_ple; rewrite <- th with arg | apply th with arg | ple; apply th with arg | split_ple; apply th with arg]; try smt_trivial.*)
+  smt_use_with th arg; if_not_done (try smt_trivial).
+Tactic Notation "smt_app_with2" constr(th) constr(arg) constr(arg2) :=
+  (**try first [ rewrite th with arg arg2 | ple; rewrite th with arg arg2 | split_ple; rewrite th with arg arg2 | rewrite <- th with arg arg2 | ple; rewrite <- th with arg arg2 | split_ple; rewrite <- th with arg arg2 | apply th with arg arg2 | ple; apply th with arg arg2 | split_ple; apply th with arg arg2]; try smt_trivial.*)
+  smt_use_with2 th arg arg2; if_not_done (try smt_trivial).
+Tactic Notation "smt_app_with3" constr(th) constr(arg) constr(arg2) constr(arg3) :=
+  (**try first [ rewrite th with arg arg2 arg3 | ple; rewrite th with arg arg2 arg3 | split_ple; rewrite th with arg arg2 arg3 | rewrite <- th with arg arg2 arg3 | ple; rewrite <- th with arg arg2 arg3 | split_ple; rewrite <- th with arg arg2 arg3 | apply th with arg arg2 arg3 | ple; apply th with arg arg2 arg3 | split_ple; apply th with arg arg2 arg3]; try smt_trivial.*)
+  smt_use_with3 th arg arg2 arg3; if_not_done (try smt_trivial).
+
+
+(** For some reason the below tactic doesn't actually work,
+   instead producing "variable m unbound" errors when used *)
+Tactic Notation "induction_on2" constr(m) constr(n) :=
+  generalize dependent n; generalize dependent m;
+  induction m; induction n; try smt_trivial.
+
+
+
+Ltac smt_app_ih IH :=
+  if_not_done (tryif intros_ple then smt_app_ih IH else smt_app IH).
+  (** split_ple; match goal with
+  | [ |- _ = _ -> _] =>
+      let H' := fresh "H" in
+      intros H'; try injectivity_in H'; simpl in H'; (*destruct H' as [->];*) smt_app_ih IH
+  | [ |- ?F -> ?G ] =>
+      let H' := fresh "H" in
+      intros H'; try injectivity_in H'; simpl in H'; smt_app_ih IH
+  | [ |- _ ] => smt_app IH
+  end.*)
+
+Ltac smt_done := try ple; try smt_trivial. (*try snipe*)
